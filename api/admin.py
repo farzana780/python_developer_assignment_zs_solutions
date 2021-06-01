e4f950e230014bb08e7a7d3017d37e57e04474dd
Farzana <farzanaece2k15@gmail.com>
2021-06-01 22:28:24 +0600

    Created Country, State and Address tables

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Country, State, Address
 
 # Register your models here.
+admin.site.register(Country)
+admin.site.register(State)
+admin.site.register(Address)
+
+
--- api/models.py
+++ api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Country(models.Model):
+    Name = models.CharField(max_length=255)
+    Latitude = models.FloatField()
+    Longitude = models.FloatField()
+    Code = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.Name
+
+class State(models.Model):
+    Name = models.CharField(max_length=255)
+    country = models.ForeignKey(Country, related_name='state', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Name
+
+class Address(models.Model):
+    Name = models.CharField(max_length=255)
+    House_number = models.CharField(max_length=255)
+    Road_number = models.IntegerField()
+    state = models.ForeignKey(State, related_name='address',  on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Name
